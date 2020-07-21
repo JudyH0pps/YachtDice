81c1adc79c1c2c29a3733d0c5c5e923acab3d9b7
김시영 <ssks1013@gmail.com>
2020-07-21 22:39:47 +0900

    Score Board 완성

---
+++ src/components/board.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="board">
+    <h1>Yacht Dice</h1>
+    <score/>
+  </div>
+</template>
+
+<script>
+import score from './score.vue'
+
+export default {
+  name: 'board',
+  components: {
+    score,
+  },
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.board {
+  margin: 0 10vw 0 10vw;
+}
+</style>
---
+++ src/components/score.vue
@@ -0,0 +1,146 @@
+<template>
+  <div class="paper">
+    <table>
+      <tr>
+        <th class="black">Categories</th>
+        <th>P1</th>
+        <th>P2</th>
+      </tr>
+      <tr>
+        <td>Ones</td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>Deuces</td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>Threes</td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>Fours</td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>Fives</td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>Sixes</td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td class="black">Subtotal</td>
+        <td class="gray"></td>
+        <td class="gray"></td>
+      </tr>
+      <tr>
+        <th class="black updot">+35 Bonus</th>
+        <td class="gray updot"></td>
+        <td class="gray updot"></td>
+      </tr>
+      <small>보너스</small>
+      <tr>
+        <td class="bottomlabel">Choice</td>
+        <td></td>
+        <td></td>
+      </tr>
+      <hr>
+      <tr>
+        <td class="bottomlabel">4 of a Kind</td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td class="bottomlabel">Full House</td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td class="bottomlabel">S. Straight</td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td class="bottomlabel">L. Straight</td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td class="bottomlabel">Yacht</td>
+        <td></td>
+        <td></td>
+      </tr>
+      <hr>
+      <tr>
+        <td class="black">Total</td>
+        <td class="gray"></td>
+        <td class="gray"></td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'score',
+    data: () => {
+        return {
+
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+.updot{
+    border-top: 2px white dotted !important;
+}
+.paper{
+    display:inline-block;
+    background:#eeeeee;
+}
+table{
+    margin: 15px 15px 15px 15px;
+    border-collapse: collapse;
+}
+.black{
+    background: #696969;
+    border: 1px solid #696969;
+    color: white;
+}
+.gray{
+    background: gray;
+    border: 1px solid #696969;
+    color: white;
+}
+hr{
+    border: none;
+    margin: 2px;
+    width: 0;
+}
+.bottomlabel{
+    border-right: double;
+}
+tr,
+th,
+td{
+    background: white;
+    border: 1px solid black;
+    padding: 5px;
+}
+tr{
+    height: 30px;
+}
+th, td{
+    width: 50px;
+}
+</style>
